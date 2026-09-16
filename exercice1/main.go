7510d8fcb42d3a9449befecf6cf6e66405c49532
Enzo Zaragoci <enzo.zaragoci@ynov.com>
2026-09-16 14:49:30 +0200

    exercice1

---
+++ exercice1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Soldat struct {
+	nom     string
+	vie     int
+	attaque int
+}
+
+func main() {
+	equipe := [6]Soldat{
+		{"Arthas", 1200, 250},
+		{"Kael", 850, 320},
+		{"Thrall", 1500, 180},
+		{"Sylvanas", 700, 400},
+		{"Garrosh", 1000, 280},
+		{"Jaina", 500, 450},
+	}
+	afficherEquipe(equipe)
+}
+
+func afficherEquipe(equipe [6]Soldat) {
+	fmt.Println("=== EQUIPE ===")
+	fmt.Println()
+
+	for i := 0; i < 6; i++ {
+		fmt.Println(equipe[i].nom)
+		fmt.Println("Vie :", equipe[i].vie)
+		fmt.Println("Attaque :", equipe[i].attaque)
+		fmt.Println()
+	}
+}
